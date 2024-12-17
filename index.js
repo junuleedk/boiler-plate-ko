e724f4ecb7fa5e6c38b897a21cafefc12447c97e
joonwoo <junulee.dk@gmail.com>
2024-12-17 20:39:37 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+const {User} = require("./models/Users");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://junu:1234@boilerplate.jlzuo.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate').then(() => console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~~안녕하세요')
+  res.send('Hello World!~~안녕하세요 좋으네')
+})
+
+app.post('/register', async (req, res) => {
+    //회원가입 할 때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+ 
+    const user = new User(req.body)
+    await user.save()  // 콜백 함수 제거하고 await 사용
+    return res.status(200).json({
+        success: true
+    })
+  
 })
 
 app.listen(port, () => {
